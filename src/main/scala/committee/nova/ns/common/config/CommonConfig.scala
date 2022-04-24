e3b90185d532fbf32a35b21c015931414b5cd1c4
MikhailTapio <mikhailtapio13468@163.com>
2022-04-24 22:45:51 +0800

    Port to 1.9

---
+++ src/main/scala/committee/nova/ns/common/config/CommonConfig.scala
@@ -0,0 +1,28 @@
+package committee.nova.ns.common.config
+
+import committee.nova.ns.common.config.CommonConfig._
+import net.minecraftforge.common.config.Configuration
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
+import org.apache.logging.log4j.Logger
+
+object CommonConfig {
+  var refreshInterval: Int = _
+  var expireTime: Int = _
+  var influencedByBiome: Boolean = _
+  var possibilityMultiplier: Float = _
+  var respectVanillaCriteria: Boolean = _
+  private var config: Configuration = _
+  private var logger: Logger = _
+}
+
+class CommonConfig(event: FMLPreInitializationEvent) {
+  logger = event.getModLog
+  config = new Configuration(event.getSuggestedConfigurationFile)
+  config.load()
+  refreshInterval = config.getInt("refresh_interval", Configuration.CATEGORY_GENERAL, 100, 20, 6000, "Refresh interval for rotten flesh catalysis. A smaller value will make it faster for a plant to be tried to catalyze if possible. Default is 100 ticks(5 sec).")
+  expireTime = config.getInt("expireTime", Configuration.CATEGORY_GENERAL, 6000, 20, 36000, "Expire time of a rotten flesh itemEntity. If smaller than the refreshInterval, the itemEntity will disappear when it refreshes for the first time.")
+  influencedByBiome = config.getBoolean("biome_influence", Configuration.CATEGORY_GENERAL, true, "Should the possibility of catalysis be influenced by biome properties (temperature & rainfall)?")
+  possibilityMultiplier = config.getFloat("possibility_multiplier", Configuration.CATEGORY_GENERAL, 1F, Float.MinValue, Float.MaxValue / 2, "The possibility multiplier. The greater the value is, the more the possibility the plant is catalyzed.")
+  respectVanillaCriteria = config.getBoolean("respectVanillaCriteria", Configuration.CATEGORY_GENERAL, true, "Should we respect vanilla's bone meal catalysis criteria? A example is when you try to bone-meal a tree, It has a 45% chance of growing.")
+  config.save()
+}
---
+++ src/main/scala/committee/nova/ns/common/util/Utilities.scala
@@ -0,0 +1,41 @@
+package committee.nova.ns.common.util
+
+import committee.nova.ns.common.config.CommonConfig._
+import net.minecraft.block.{Block, IGrowable}
+import net.minecraft.entity.item.EntityItem
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
+
+object Utilities {
+  def catalyze(entity: EntityItem): Boolean = {
+    val world = entity.worldObj
+    val plantPos = entity.getPosition.add(0, 0.2, 0)
+    val blockIn = world.getBlockState(plantPos).getBlock
+    if (influencedByBiome && !willTryCatalyze(world, plantPos)) return false
+    if (tryCatalyze(world, blockIn, plantPos)) return true
+    val dirtPos = plantPos.down()
+    val dirt = world.getBlockState(dirtPos).getBlock
+    tryCatalyze(world, dirt, dirtPos)
+  }
+
+  def tryCatalyze(world: World, block: Block, pos: BlockPos): Boolean = {
+    if (!block.isInstanceOf[IGrowable]) return false
+    val plant = block.asInstanceOf[IGrowable]
+    try {
+      if (!plant.canGrow(world, pos, world.getBlockState(pos), world.isRemote)) return false
+      if (respectVanillaCriteria && !plant.canUseBonemeal(world, world.rand, pos, world.getBlockState(pos))) return false
+      plant.grow(world, world.rand, pos, world.getBlockState(pos))
+    } catch {
+      case _: Exception => return false
+    }
+    true
+  }
+
+  def willTryCatalyze(world: World, pos: BlockPos): Boolean = {
+    val biome = world.getBiomeGenForCoords(pos)
+    val r = world.rand
+    val temperature = biome.getTemperature
+    val humidity = biome.getRainfall
+    possibilityMultiplier * (temperature * Math.abs(temperature) * (r.nextFloat() - 0.3F) + humidity * Math.abs(humidity) * (r.nextFloat() - 0.3F)) > 0.25F
+  }
+}
