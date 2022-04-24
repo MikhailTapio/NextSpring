40c5ee8512d1bc286a277971e017484e3c0af1d7
MikhailTapio <mikhailtapio13468@163.com>
2022-04-24 19:00:04 +0800

    Port to 1.8

---
+++ src/main/scala/committee/nova/ns/common/event/NextSpringEvent.scala
@@ -0,0 +1,42 @@
+package committee.nova.ns.common.event
+
+import committee.nova.ns.common.config.CommonConfig
+import committee.nova.ns.common.util.Utilities.catalyze
+import net.minecraft.entity.item.EntityItem
+import net.minecraft.init.Items
+import net.minecraftforge.event.entity.EntityJoinWorldEvent
+import net.minecraftforge.event.entity.item.ItemExpireEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
+class NextSpringEvent {
+  @SubscribeEvent
+  def onItemDropped(event: EntityJoinWorldEvent): Unit = {
+    if (event.isCanceled) return
+    if (!event.entity.isInstanceOf[EntityItem]) return
+    val itemEntity = event.entity.asInstanceOf[EntityItem]
+    val stack = itemEntity.getEntityItem
+    if (stack == null) return
+    if (stack.getItem != Items.rotten_flesh) return
+    itemEntity.lifespan = CommonConfig.refreshInterval
+  }
+
+  @SubscribeEvent
+  def onItemExpire(event: ItemExpireEvent): Unit = {
+    if (event.isCanceled) return
+    val itemEntity = event.entityItem
+    val stack = itemEntity.getEntityItem
+    if (stack.getItem != Items.rotten_flesh) return
+    if (itemEntity.ticksExisted >= CommonConfig.expireTime) return
+    if (!catalyze(itemEntity)) {
+      event.extraLife = CommonConfig.refreshInterval
+      event.setCanceled(true)
+      return
+    }
+    val newStack = stack.copy()
+    newStack.stackSize -= 1
+    if (newStack.stackSize <= 0) return
+    itemEntity.setEntityItemStack(newStack)
+    event.extraLife = CommonConfig.refreshInterval
+    event.setCanceled(true)
+  }
+}
